a94afe437d8532c901e46c0187090a5144c8c4d0
Justin Vega <vegajustin26@gmail.com>
2020-09-14 10:51:23 -0400

    spect model & statinf notebook init

--- notebooks/Justin's Notes/brighttemp.py
+++ notebooks/Justin's Notes/brighttemp.py
@@ -8,6 +8,7 @@ def brighttemp(sb, beam, freq, RJ=False): #sb is in Jy/beam
     boltzc = 1.380649e-16 # in erg/K
     sol = 2.99792e10 #in cm/s
 
+    global SI_area
     #Calculate beam area in SI and CGS
     SI_area = np.pi*bmaj*bmin/(4*np.log(2)) # in arcsec^2 units
     sr_area = SI_area*((np.pi/180)/3600)**2 #in sr; 2.35e-11 str = 1 arcsec^2
